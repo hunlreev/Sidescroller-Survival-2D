5aa4c8a427e5b299f9d0782858b07d80542f2702
Hunter Reeves <hunlreev@ut.utm.edu>
2019-09-05 20:36:48 -0500

    Add project files.

---
+++ Project_352/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Project_352
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
